75cc1a47233acc98806efdcec2fb038826a73f16
Heitor Sampaio <heitorosampaio@gmail.com>
2022-05-25 23:08:08 -0300

    Trying to fix error 500 on production

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,21 +3,24 @@ import axios from 'axios'
 let url;
 
 if (process.env.NODE_ENV === 'production') {
+  url = process.env.NEXT_PUBLIC_API_URL
+
   if (process.env.VERCEL) {
-    url = process.env.NEXT_PUBLIC_VERCEL_URL
+    console.log("VERCEL")
   }
 
   if (process.env.NETLIFY) {
-    url = process.env.NEXT_PUBLIC_URL
+    console.log("NETLIFY")
   }
 
   if (!process.env.VERCEL && !process.env.NETLIFY) {
-    url = process.env.NEXT_PUBLIC_LOCAL_API_URL
+    url = "http://localhost:3000"
+    console.log("LOCAL")
   }
 }
 
 if (process.env.NODE_ENV === 'development') {
-  url = process.env.NEXT_PUBLIC_LOCAL_API_URL
+  url = "http://localhost:3000"
 }
 
 export const api = axios.create({
